b92576787ff7596a0a7b5ee983910795c86fdfe9
ajp64 <antonyjparnell@gmail.com>
2026-03-03 14:38:25 +1300

    Add calculation logic to service

--- src/main/java/com/hnry/taxCalculator/service/CalculationService.java
+++ src/main/java/com/hnry/taxCalculator/service/CalculationService.java
@@ -1,13 +1,95 @@
 package com.hnry.taxCalculator.service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class CalculationService {
 
-    public BigDecimal calculateIncometax(String untaxedIncome)
-    {
-        BigDecimal incomeAsBigDecimal = new BigDecimal(untaxedIncome);
+    private static final BigDecimal MAX_BAND_A_INCOME = BigDecimal.valueOf(15600);
+    private static final BigDecimal MAX_BAND_B_INCOME = BigDecimal.valueOf(53500);
+    private static final BigDecimal MAX_BAND_C_INCOME = BigDecimal.valueOf(78100);
+    private static final BigDecimal MAX_BAND_D_INCOME = BigDecimal.valueOf(180000);
+    private static final BigDecimal MIN_BAND_B_TAX = BigDecimal.valueOf(1638);
+    private static final BigDecimal MIN_BAND_C_TAX = BigDecimal.valueOf(8270.5);
+    private static final BigDecimal MIN_BAND_D_TAX = BigDecimal.valueOf(15650.5);
+    private static final BigDecimal MIN_BAND_E_TAX = BigDecimal.valueOf(49277.5);
 
-        return incomeAsBigDecimal.multiply(BigDecimal.valueOf(0.105));
+    public BigDecimal calculateIncomeTax(String untaxedIncome)
+    {
+        BigDecimal incomeAsBigDecimal = new BigDecimal(0);
+
+        try
+        {
+            incomeAsBigDecimal = new BigDecimal(untaxedIncome);
+        }
+
+        catch (Exception e) {
+            System.out.println("Please submit a value with only 0-9 and a single decimal place.");
+        }
+
+        if (incomeAsBigDecimal.compareTo(MAX_BAND_A_INCOME) <= 0)
+        {
+            return calculateForTaxBandA(incomeAsBigDecimal).setScale(2, RoundingMode.HALF_UP);
+        }
+
+        if (incomeAsBigDecimal.compareTo(MAX_BAND_B_INCOME) <= 0)
+        {
+            return calculateForTaxBandB(incomeAsBigDecimal).setScale(2, RoundingMode.HALF_UP);
+        }
+
+        if (incomeAsBigDecimal.compareTo(MAX_BAND_C_INCOME) <= 0)
+        {
+            return calculateForTaxBandC(incomeAsBigDecimal).setScale(2, RoundingMode.HALF_UP);
+        }
+
+        if (incomeAsBigDecimal.compareTo(MAX_BAND_D_INCOME) <= 0)
+        {
+            return calculateForTaxBandD(incomeAsBigDecimal).setScale(2, RoundingMode.HALF_UP);
+        }
+
+        return calculateForTaxBandE(incomeAsBigDecimal).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateForTaxBandA(BigDecimal income)
+    {
+        return income.multiply(BigDecimal.valueOf(0.105));
+    }
+
+    private BigDecimal calculateForTaxBandB(BigDecimal income)
+    {
+        BigDecimal isolateBracket = income.subtract(MAX_BAND_A_INCOME);
+
+        BigDecimal bracketBTax = isolateBracket.multiply(BigDecimal.valueOf(0.175));
+
+        return bracketBTax.add(MIN_BAND_B_TAX);
+    }
+
+    private BigDecimal calculateForTaxBandC(BigDecimal income)
+    {
+        BigDecimal isolateBracket = income.subtract(MAX_BAND_B_INCOME);
+
+        BigDecimal bracketCTax = isolateBracket.multiply(BigDecimal.valueOf(0.30));
+
+        return bracketCTax.add(MIN_BAND_C_TAX);
+    }
+
+    private BigDecimal calculateForTaxBandD(BigDecimal income)
+    {
+        BigDecimal isolateBracket = income.subtract(MAX_BAND_C_INCOME);
+
+        BigDecimal bracketCTax = isolateBracket.multiply(BigDecimal.valueOf(0.33));
+
+        return bracketCTax.add(MIN_BAND_D_TAX);
+    }
+
+    private BigDecimal calculateForTaxBandE(BigDecimal income)
+    {
+        BigDecimal isolateBracket = income.subtract(MAX_BAND_D_INCOME);
+
+        BigDecimal bracketCTax = isolateBracket.multiply(BigDecimal.valueOf(0.39));
+
+        BigDecimal sum = bracketCTax.add(MIN_BAND_E_TAX);
+
+        return bracketCTax.add(MIN_BAND_E_TAX);
     }
 }
--- src/test/java/com/hnry/taxCalculator/serviceTests/CalculationServiceTests.java
+++ src/test/java/com/hnry/taxCalculator/serviceTests/CalculationServiceTests.java
@@ -20,33 +20,33 @@ public class CalculationServiceTests {
     @Test
     void testIncome10000()
     {
-        BigDecimal actual = testSubject.calculateIncometax("10000");
+        BigDecimal actual = testSubject.calculateIncomeTax("10000");
 
-        assertEquals(new BigDecimal("1050.000"), actual);
+        assertEquals(new BigDecimal("1050.00"), actual);
     }
 
     @Test
     void testIncome35000()
     {
-        BigDecimal actual = testSubject.calculateIncometax("35000");
+        BigDecimal actual = testSubject.calculateIncomeTax("35000");
 
-        assertEquals(new BigDecimal("5033.000"), actual);
+        assertEquals(new BigDecimal("5033.00"), actual);
     }
 
     @Test
     void testIncome100000()
     {
-        BigDecimal actual = testSubject.calculateIncometax("100000");
+        BigDecimal actual = testSubject.calculateIncomeTax("100000");
 
-        assertEquals(new BigDecimal("22877.500"), actual);
+        assertEquals(new BigDecimal("22877.50"), actual);
     }
 
     @Test
     void testIncome220000()
     {
-        BigDecimal actual = testSubject.calculateIncometax("220000");
+        BigDecimal actual = testSubject.calculateIncomeTax("220000");
 
-        assertEquals(new BigDecimal("64877.500"), actual);
+        assertEquals(new BigDecimal("64877.50"), actual);
     }
 
 }
